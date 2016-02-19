e52b4daa5eb82b0edf435314308d2753ebdbf636
Shashwat Anand <shashwat.anand@gmail.com>
2016-02-19 23:17:59 +0530

    Get data from get request

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,28 +3,10 @@
 }*/
 
 var meanApp = angular.module("meanApp", []);
-meanApp.controller('AppCtrl', ['$scope', '$http', function($scope){
-	console.log("Hello world from controller");
+meanApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
+	//console.log("Hello world from controller");
 
-
-	student1 = {
-		firstName: 'Manish',
-		lastName: 'Chahar',
-		id: '1'
-	};
-
-	student2 = {
-		firstName: 'Shashwat',
-		lastName: 'Anand',
-		id: '4'
-	};
-
-	student3 = {
-		firstName: 'Shubhangi',
-		lastName: 'Sinha',
-		id: '2'
-	};
-
-	var studentList = [student1, student2, student3];
-	$scope.studentList = studentList;
+	$http.get('/studentList').success(function(response){
+		$scope.studentList = response;
+	});
 }]);
--- server.js
+++ server.js
@@ -7,5 +7,29 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/studentList', function(request, response){
+	console.log("Received a GET request");
+	student1 = {
+		firstName: 'Manish',
+		lastName: 'Chahar',
+		id: '1'
+	};
+
+	student2 = {
+		firstName: 'Shashwat',
+		lastName: 'Anand',
+		id: '4'
+	};
+
+	student3 = {
+		firstName: 'Shubhangi',
+		lastName: 'Sinha',
+		id: '2'
+	};
+
+	var studentList = [student1, student2, student3];
+	response.json(studentList);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
