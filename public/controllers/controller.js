8e53d924ed08330c40691627b6dafb6a607ea5bc
Shashwat Anand <shashwat.anand@gmail.com>
2016-02-19 23:07:57 +0530

    showing data from controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,6 +3,28 @@
 }*/
 
 var meanApp = angular.module("meanApp", []);
-meanApp.controller('AppCtrl', ['$scope', '$http', function(){
+meanApp.controller('AppCtrl', ['$scope', '$http', function($scope){
 	console.log("Hello world from controller");
+
+
+	student1 = {
+		firstName: 'Manish',
+		lastName: 'Chahar',
+		id: '1'
+	};
+
+	student2 = {
+		firstName: 'Shashwat',
+		lastName: 'Anand',
+		id: '4'
+	};
+
+	student3 = {
+		firstName: 'Shubhangi',
+		lastName: 'Sinha',
+		id: '2'
+	};
+
+	var studentList = [student1, student2, student3];
+	$scope.studentList = studentList;
 }]);
